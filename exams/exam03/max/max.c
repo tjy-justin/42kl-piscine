22867862e2ba0545ebf6edb06da2738d87144c17
Jin Yi Tan <jin-tan@u80z04s02.42kl.edu.my>
2024-04-04 23:59:37 +0800

    e3

---
+++ exams/exam03/max/max.c
@@ -0,0 +1,35 @@
+// tab = array of int
+// len = # of elements
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int	max(int *tab, unsigned int len)
+{
+	int	max;
+
+	if (!len)
+		return (0);
+	max = tab[len--];
+	while (len--)
+	{
+		if (tab[len] > max)
+		{
+			max = tab[len];
+		}
+	}
+	return (max);
+}
+
+int	main(void)
+{
+	int *tab;
+
+	if (!(tab = (int *)malloc(sizeof(int) * 3)))
+		return (0);
+	tab[0] = 7;
+	tab[1] = 6;
+	tab[2] = 4;
+	printf("%d", max(tab, 3));
+	return (0);
+}
