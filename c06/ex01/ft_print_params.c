13928272f80dc9d1ef9e34a11993d051e2619f41
Jin Yi Tan <jin-tan@u80z04s02.42kl.edu.my>
2024-04-04 14:36:51 +0800

    6

--- c06/ex01/ft_print_params.c
+++ c06/ex01/ft_print_params.c
@@ -1,3 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jin-tan <jin-tan@student.42kl.edu.my>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/04 13:51:32 by jin-tan           #+#    #+#             */
+/*   Updated: 2024/04/04 14:09:41 by jin-tan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// iterates from av[] 1, 2, ...
+
 #include <unistd.h>
 
 void	ft_putchar(char c)
@@ -7,20 +21,23 @@ void	ft_putchar(char c)
 
 int	main(int ac, char *av[])
 {
-	int i;
-	int j;
+	int	i;
+	int	j;
 
-	i = 0;
-	j = 0;
-	if (ac > 0)
+	i = 1;
+	if (ac > 1)
 	{
-		while (av[i][j]) // iterates from av[] 1, 2, ...
+		while (i < ac)
 		{
-			ft_putchar(av[i][j]);
-			j++;
+			j = 0;
+			while (av[i][j])
+			{
+				ft_putchar(av[i][j]);
+				j++;
+			}
+			i++;
+			ft_putchar('\n');
 		}
-		i++;
-		ft_putchar('\n');
 	}
 	return (0);
 }
--- c06/ex02/ft_rev_params.c
+++ c06/ex02/ft_rev_params.c
@@ -1,3 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_params.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jin-tan <jin-tan@student.42kl.edu.my>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/04 13:51:34 by jin-tan           #+#    #+#             */
+/*   Updated: 2024/04/04 14:00:19 by jin-tan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// iterates from av[] 2, 1, ...
+
 #include <unistd.h>
 
 void	ft_putchar(char c)
@@ -7,15 +21,14 @@ void	ft_putchar(char c)
 
 int	main(int ac, char *av[])
 {
-	int i;
-	int j;
+	int	i;
+	int	j;
 
 	i = ac - 1;
-
 	while (i > 0)
 	{
 		j = 0;
-		while (av[i][j]) // iterates from av[] 2, 1, ...
+		while (av[i][j])
 		{
 			ft_putchar(av[i][j]);
 			j++;
